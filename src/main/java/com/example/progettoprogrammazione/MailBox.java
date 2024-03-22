4b0eeda3601f0b6254e338a94b0890c9f6199e2e
Mariusss5 <dumitriumarius5@gmail.com>
2024-03-22 12:45:51 +0100

    Proviamo a creare il model del client xdddddd

---
+++ src/main/java/com/example/progettoprogrammazione/MailBox.java
@@ -0,0 +1,53 @@
+package com.example.progettoprogrammazione;
+
+import javafx.beans.property.SimpleStringProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailBox {
+
+    private List<Email> inMail ;
+    private String account;
+    private SimpleStringProperty mittenteProperty;
+
+    private SimpleStringProperty destinatarioProperty;
+
+    private SimpleStringProperty oggettoProperty;
+
+    private SimpleStringProperty messageProperty;
+
+    public MailBox(String a){
+
+        inMail = new ArrayList<Email>();
+
+        this.account = a;
+        mittenteProperty = new SimpleStringProperty();
+        destinatarioProperty = new SimpleStringProperty();
+        oggettoProperty = new SimpleStringProperty();
+        messageProperty = new SimpleStringProperty();
+    }
+    public SimpleStringProperty getMittenteProperty(){return mittenteProperty;}
+    public SimpleStringProperty getDestinatarioProperty(){return destinatarioProperty;}
+    public SimpleStringProperty getOggettoProperty(){return oggettoProperty;}
+    public SimpleStringProperty getMessageProperty(){return messageProperty;}
+
+
+    public void aggiungiEmail(Email EmailDaAggiungere){
+        inMail.add(EmailDaAggiungere);
+    }
+    public void rimuoviEmail(Email EmailDaEliminare){
+        inMail.remove(EmailDaEliminare);
+    }
+
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
+
+
