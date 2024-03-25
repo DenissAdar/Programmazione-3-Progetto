3ef2101fe43f562539e3104b45d288d3ad0ab9b8
gioiadidii <164083059+gioiadidii@users.noreply.github.com>
2024-03-25 11:24:58 +0100

    Aggiunto due cose sulle property e ObservableList (obbligatorie dal prof)

--- src/main/java/com/example/progettoprogrammazione/Client.java
+++ src/main/java/com/example/progettoprogrammazione/Client.java
@@ -1,42 +1,75 @@
 package com.example.progettoprogrammazione;
 
 import com.example.progettoprogrammazione.Email;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 
 
 public class Client {
 
-    private List<Email> inMail;
-    private List<Email> outMail;
+    Socket socket = null;
+    private String jsonFilePath;
+    private ListProperty<Email> inMail;
+    private ListProperty<Email> outMail;
+    private ObservableList<Email> inMailContent;
+    private ObservableList<Email> outMailContent;
+
     private String account;
     private SimpleStringProperty senderProperty;
     private SimpleStringProperty receiverProperty;
     private SimpleStringProperty objectProperty;
     private SimpleStringProperty messageProperty;
 
-    /*Costruttore di un nuovo Servizio di Mail, conosciuto anche come MailBox, */
-    public Client(String account, String jsonFilePath){
+    public Client(String account, String JFilePath){
         this.account = account;
+        this.jsonFilePath = JFilePath;
+
         senderProperty = new SimpleStringProperty();
         receiverProperty = new SimpleStringProperty();
         objectProperty = new SimpleStringProperty();
         messageProperty = new SimpleStringProperty();
-        inMail = new ArrayList<>();
-        outMail = new ArrayList<>();
 
-        // Leggi il file JSON e aggiungi le email al client
+        inMail = new SimpleListProperty<>(inMailContent);
+        inMailContent = FXCollections.observableList(new LinkedList<>());
+
+
+        outMail = new SimpleListProperty<>(outMailContent);
+        outMailContent = FXCollections.observableList(new LinkedList<>());
+
+        inMailContent.addListener(new ListChangeListener<Email>() {
+            @Override
+            public void onChanged(Change<? extends Email> change) {
+
+            }
+        });
+
+        outMailContent.addListener(new ListChangeListener<Email>() {
+            @Override
+            public void onChanged(Change<? extends Email> change) {
+
+            }
+        });
+
+
+        // Legge il file JSON e aggiunge le email al client
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(new File("src/main/java/com/example/progettoprogrammazione/accounts/account.json"));
+            JsonNode rootNode = objectMapper.readTree(new File(jsonFilePath));
 
             // Itera sui nodi del file JSON
             for (JsonNode emailNode : rootNode) {
@@ -51,7 +84,7 @@ public class Client {
                     String message = contentNode.get("text").asText();
                     String dateTime = contentNode.get("dateTime").asText();
 
-                    // Crea un oggetto Email e aggiungilo alla lista inMail
+                    // Crea un oggetto Email e lo aggiunge alla lista corretta
                     Email emailObj = new Email(sender, receiver, object, message, dateTime);
 
                     /* TODO - Ci dovrebbero essere una serie di if per controllare se il destinatario è l'account
@@ -62,7 +95,6 @@ public class Client {
                     if(Objects.equals(this.account, sender)) outMail.add(emailObj);
                     else if(Objects.equals(this.account, receiver)) inMail.add(emailObj);
 
-                   // inMail.add(emailObj); // Se è una mail in entrata
                 }
 
             }
@@ -75,11 +107,11 @@ public class Client {
     public SimpleStringProperty getReceiverProperty(){return receiverProperty;}
     public SimpleStringProperty getObjectProperty(){return objectProperty;}
     public SimpleStringProperty getMessageProperty(){return messageProperty;}
-    public List<Email> getInMail() {
+    public ListProperty<Email> getInMail() {
         return inMail;
     }
     //Aggiunto da DEN quando ho creato il metodo di controllo per l'inserimento in Lista
-    public List<Email> getOutMail() {
+    public ListProperty<Email> getOutMail() {
         return outMail;
     }
 
