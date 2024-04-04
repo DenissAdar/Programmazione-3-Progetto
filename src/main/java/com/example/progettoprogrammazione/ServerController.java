cd0112c742d86afbc0e127b10c65f4a0f9cb6f74
DenisAdar <deniss.adar@gmail.com>
2024-04-04 18:21:17 +0200

    Dipi

--- src/main/java/com/example/progettoprogrammazione/ServerController.java
+++ src/main/java/com/example/progettoprogrammazione/ServerController.java
@@ -1,6 +1,7 @@
 package com.example.progettoprogrammazione;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
@@ -11,10 +12,14 @@ public class ServerController {
     @FXML
     private TextArea ServerLog;
     private Server server ;
+    @FXML
+    ListView<String> serverList;
+
     public void init()
     {
         server = new Server();
-        server.listen(6000);
+        serverList.itemsProperty().bind(server.getLogList());
+        //server.listen(6000);
         //ServerLog.textProperty().bind(server.getAccountLogProperty());
     }
 }
